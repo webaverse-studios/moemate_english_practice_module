24e8df837fcde4388c5861358e17b3b2772a5138
gonnavis <gonnavis@163.com>
2023-08-13 16:29:51 +0800

    ok: check_answer.

--- scripts/english_practice.js
+++ scripts/english_practice.js
@@ -4,10 +4,17 @@ function _handleCreateQuestionSkill(event) {
     // window.companion.SendMessage({ type: "TEXT", user: event.name, value: event.value, timestamp: Date.now(), alt: responseObj.description});
     // window.companion.SendMessage({ type: "TEXT", user: event.name, value: event.value, timestamp: Date.now(), alt: 'alt'});
     setTimeout(() => {
-        window.hooks.emit("hack_delay", `You created this question ${event.value}, just write it as it, don't change anything, don't speak anything else!`);
+        window.hooks.emit("hack_delay", `You created this question {${event.value}}, just write it as is, don't change anything, don't speak anything else!`);
+    }, 100);
+}
+
+function _handleCheckAnswerSkill(event) {
+    setTimeout(() => {
+        window.hooks.emit("hack_delay", `Check if user's answer is correct, and explain why it is correct or not.`);
     }, 100);
 }
 
 export function init() {
     window.hooks.on('english_practice:handle_create_question_skill', _handleCreateQuestionSkill)
+    window.hooks.on('english_practice:handle_check_answer_skill', _handleCheckAnswerSkill)
 }
