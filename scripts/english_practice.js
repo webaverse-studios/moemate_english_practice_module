e2eabd2c3c4ac737643e8e3843faa35ed3ddee26
gonnavis <gonnavis@163.com>
2023-08-16 17:40:40 +0800

    ok: speak answer and explanations.

--- scripts/english_practice.js
+++ scripts/english_practice.js
@@ -1,14 +1,17 @@
 
+let lastQuestionNAnswer = '';
+
 function _handleCreateQuestionSkill(event) {
     console.log(`_handleCreateQuestionSkill(event):`, event);
-    debugger
+    // debugger
     // window.companion.SendMessage({ type: "TEXT", user: event.name, value: event.value, timestamp: Date.now(), alt: responseObj.description});
     // window.companion.SendMessage({ type: "TEXT", user: event.name, value: event.value, timestamp: Date.now(), alt: 'alt'});
     // setTimeout(() => {
     //     window.hooks.emit("hack_delay", `You created this question {${event.value}}, don't answer the question, just write the question as is (keep the blank!), don't change anything, don't speak anything else!`);
     // }, 100);
 
-    const question = event.value.split('------')[0];
+    lastQuestionNAnswer = event.value;
+    const question = lastQuestionNAnswer.split('------')[0];
     // window.companion.SendMessage({ type: "TEXT", user: event.name, value: question, timestamp: Date.now(), alt: 'alt'});
     setTimeout(() => {
         window.hooks.emit("hack_delay", `You created this question {${question}}, don't answer the question, just write the question and (keep the blank!) choices as is, don't change anything, don't speak anything else!`);
@@ -16,8 +19,9 @@ function _handleCreateQuestionSkill(event) {
 }
 
 function _handleCheckAnswerSkill(event) {
+    const answer = lastQuestionNAnswer.split('------')[1];
     setTimeout(() => {
-        window.hooks.emit("hack_delay", `Check if user's answer is correct, and explain why it is correct or not.`);
+        window.hooks.emit("hack_delay", `Wrint down the answer and explanations: {${answer}}. Just write the answer and explanations in between \`{}\` as is, don't change anything, don't speak anything else!`);
     }, 100);
 }
 
