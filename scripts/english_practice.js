ae9f1740a134a1e8ecf670936d1cf256ec7cd912
gonnavis <gonnavis@163.com>
2023-08-16 16:01:43 +0800

    improve prompt.

--- scripts/english_practice.js
+++ scripts/english_practice.js
@@ -1,21 +1,25 @@
 
-// function _handleCreateQuestionSkill(event) {
-//     debugger
-//     // window.companion.SendMessage({ type: "TEXT", user: event.name, value: event.value, timestamp: Date.now(), alt: responseObj.description});
-//     // window.companion.SendMessage({ type: "TEXT", user: event.name, value: event.value, timestamp: Date.now(), alt: 'alt'});
-//     setTimeout(() => {
-//         window.hooks.emit("hack_delay", `You created this question {${event.value}}, don't answer the question, just write the question as is (keep the blank!), don't change anything, don't speak anything else!`);
-//     }, 100);
-// }
+function _handleCreateQuestionSkill(event) {
+    console.log(`_handleCreateQuestionSkill(event):`, event);
+    // debugger
+    // window.companion.SendMessage({ type: "TEXT", user: event.name, value: event.value, timestamp: Date.now(), alt: responseObj.description});
+    // window.companion.SendMessage({ type: "TEXT", user: event.name, value: event.value, timestamp: Date.now(), alt: 'alt'});
+    // setTimeout(() => {
+    //     window.hooks.emit("hack_delay", `You created this question {${event.value}}, don't answer the question, just write the question as is (keep the blank!), don't change anything, don't speak anything else!`);
+    // }, 100);
 
-// function _handleCheckAnswerSkill(event) {
-//     setTimeout(() => {
-//         window.hooks.emit("hack_delay", `Check if user's answer is correct, and explain why it is correct or not.`);
-//     }, 100);
-// }
+    const question = event.value.split('------')[0];
+    window.companion.SendMessage({ type: "TEXT", user: event.name, value: question, timestamp: Date.now(), alt: 'alt'});
+}
+
+function _handleCheckAnswerSkill(event) {
+    setTimeout(() => {
+        window.hooks.emit("hack_delay", `Check if user's answer is correct, and explain why it is correct or not.`);
+    }, 100);
+}
 
 export function init() {
-    // window.hooks.on('english_practice:handle_create_question_skill', _handleCreateQuestionSkill)
-    // window.hooks.on('english_practice:handle_check_answer_skill', _handleCheckAnswerSkill)
+    window.hooks.on('english_practice:handle_create_question_skill', _handleCreateQuestionSkill)
+    window.hooks.on('english_practice:handle_check_answer_skill', _handleCheckAnswerSkill)
     window.components.AddComponentToScreen('chat-input', 'PromptSelector');
 }
