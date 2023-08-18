fafce45aae841f0bbf3c76e2867e3d9939c3251a
gonnavis <gonnavis@163.com>
2023-08-18 22:18:44 +0800

    Restor: `PromptSelector`. & Don't provide example to prevent AI almost always create questions start with something like "By the time ...".

--- components/PromptSelector.jsx
+++ components/PromptSelector.jsx
@@ -11,6 +11,7 @@ export const PromptSelector = ({onChat, value, setValue}) => {
   const [isOpen, setIsOpen] = useState(false);
   const [level, setLevel] = useState('intermediate');
   const [type, setType] = useState('single-choice');
+  const [point, setPoint] = useState('tenses');
 
   const domActions = document.querySelector('.'+styles.actions)
 
@@ -18,13 +19,19 @@ export const PromptSelector = ({onChat, value, setValue}) => {
     setIsOpen(!isOpen);
   }
 
-  const sendPrompt = () => {
+  const sendPrompt = async () => {
     // onChat({ type: 'text', value: `Make an intermediate-level English grammar single-choice question. Don't show me the answer immediately.` });
     // onChat({ type: 'text', value: `Make an intermediate-level English grammar single-choice question. Just write the question as is, don't change anything, don't speak anything else!` });
-    onChat({ type: 'text', value: `Make an ${level}-level English grammar ${type} question. Just write the question, don't speak anything else.` });
+    // onChat({ type: 'text', value: `Make an ${level}-level English grammar ${type} question. Just write the question, don't speak anything else.` });
     // onChat({ type: 'text', value: `Make an intermediate-level English Fill-In-The-Blank question. Just write the question, don't speak anything else.` });
     // onChat({ type: 'text', value: `Explain why the answer is correct or not.` });
     // onChat({ type: 'text', value: `Explain why.` });
+    
+    window.hooks.emitSync("character:reset");
+    window.hooks.emitSync("character:processing");
+    const name = window.companion.GetCharacterAttribute('name');
+    await window.hooks.emit('english_practice:handle_create_question_skill', {name, point});
+    window.hooks.emitSync("character:standby");
   }
 
   const sendExplainWhy = () => {
@@ -46,7 +53,7 @@ export const PromptSelector = ({onChat, value, setValue}) => {
       {isOpen &&
         <div className={styles.emojiPickerWrap} style={{color:'white'}}>
           
-          <select
+          {/* <select
             value={level}
             onChange={(e) => {
               setLevel(e.target.value);
@@ -55,9 +62,9 @@ export const PromptSelector = ({onChat, value, setValue}) => {
             <option key={"elementary"} value={"elementary"}>elementary</option>
             <option key={"intermediate"} value={"intermediate"}>intermediate</option>
             <option key={"advanced"} value={"advanced"}>advanced</option>
-          </select>
+          </select> */}
 
-          <select
+          {/* <select
             value={type}
             onChange={(e) => {
               setType(e.target.value);
@@ -66,15 +73,26 @@ export const PromptSelector = ({onChat, value, setValue}) => {
             <option key={"single-choice"} value={"single-choice"}>single-choice</option>
             <option key={"multiple-choice"} value={"multiple-choice"}>multiple-choice</option>
             <option key={"fill-in-blank"} value={"fill-in-blank"}>fill-in-blank</option>
+          </select> */}
+
+          <select
+            value={point}
+            onChange={(e) => {
+              setPoint(e.target.value);
+            }}
+          >
+            <option key={"any"} value={"any"}>any</option>
+            <option key={"tenses"} value={"tenses"}>tenses</option>
+            <option key={"preposition"} value={"preposition"}>preposition</option>
           </select>
 
           <button onClick={sendPrompt}>Send</button>
 
-          <button onClick={sendExplainWhy}>Explain why</button>
+          {/* <button onClick={sendExplainWhy}>Explain why</button>
 
           <button onClick={revealAnswer}>Reveal answer</button>
 
-          <button onClick={sendAnswer}>Send answer</button>
+          <button onClick={sendAnswer}>Send answer</button> */}
 
         </div>
       }
--- scripts/english_practice.js
+++ scripts/english_practice.js
@@ -6,9 +6,18 @@ async function _handleCreateQuestionSkill(event) {
     // window.companion.SendMessage({ type: "CREATE_QUESTION", user: event.name, value: 'Creating a question', timestamp: Date.now(), alt: 'alt'});
     // window.companion.SendMessage({ type: "CREATE_QUESTION", user: event.name, value: '', timestamp: Date.now(), alt: 'alt'});
 
+    let pointAndSpace = '';
+    if (event.point) {
+        pointAndSpace = event.point === 'any' ? '' : (event.point + ' ');
+    } else {
+        pointAndSpace = 'tenses';
+    }
+
     const context = {
         // messages: `\n\nHuman:User's answer is C. Extract correct answer from following text:\nThe correct answer is B) walked. This is the correct answer because the sentence is describing an action that took place in the past (yesterday) and is completed, so the simple past tense is needed.\nA) will walk is incorrect because it is the future tense, referring to an action that will take place in the future.\nC) had walked is the past perfect tense, used to describe an action that was completed before another past action. It does not fit here.\nD) have walked is the present perfect tense, used to describe an action that started in the past but continues to or impacts the present. It does not fit the timeframe of the sentence.\nThe simple past tense \"walked\" is the only option that properly conveys that the action took place entirely in the past.\nThen check if user's answer is correct or wrong, only reply correct or wrong, don't say anything else.\n\nAssistant:`,
+
         // messages: `\n\nHuman:You are now role-playing as a senior English teacher.\nCreate an English grammar verb tense multiple choice fill-in-the-blank question.\nReveal the correct answer after the question, explain why it is correct.\nAnd explain why other choices are wrong.\nAnd don't forget to add '\\n------\\n' between the question and the explanations.\nExample:\nYesterday I _____ to the store when it started raining.\n A) will walk\n B) walked\n C) had walked\n D) have walked\n------\nThe correct answer is B) walked. This is the correct answer because the sentence is describing an action that took place in the past (yesterday) and is completed, so the simple past tense is needed.\nA) will walk is incorrect because it is the future tense, referring to an action that will take place in the future.\nC) had walked is the past perfect tense, used to describe an action that was completed before another past action. It does not fit here.\nD) have walked is the present perfect tense, used to describe an action that started in the past but continues to or impacts the present. It does not fit the timeframe of the sentence.\nThe simple past tense \"walked\" is the only option that properly conveys that the action took place entirely in the past.\n\nAssistant:`
+        
 //         messages: `\n\nHuman:
 // ### You are now role-playing as a senior English teacher. Create an English grammar verb tense multiple choice fill-in-the-blank question. ( Random seed: ${Math.random()} )
 // Don't reveal the answer.Only write the question, don't speak anything else.
@@ -21,34 +30,52 @@ async function _handleCreateQuestionSkill(event) {
 // D) have walked
 
 // Assistant:`,
+
+
+//         messages: `\n\nHuman:
+// ### Create an English grammar tenses multiple choice fill-in-the-blank question.
+// Reveal the correct answer after the question, explain why it is correct.
+// Explain why other choices are wrong.
+// Explain why you create this question.
+// Add '\\n------\\n' before the question, and between the question and the explanations.
+
+// ###Example:
+// Here is an English grammar verb tense multiple choice fill-in-the-blank question:
+// ------
+// Yesterday I _____ to the store when it started raining.
+// A) will walk
+// B) walked
+// C) had walked
+// D) have walked
+// ------
+// The correct answer is B) walked. This is the correct answer because the sentence is describing an action that took place in the past (yesterday) and is completed, so the simple past tense is needed.
+// A) will walk is incorrect because it is the future tense, referring to an action that will take place in the future.
+// C) had walked is the past perfect tense, used to describe an action that was completed before another past action. It does not fit here.
+// D) have walked is the present perfect tense, used to describe an action that started in the past but continues to or impacts the present. It does not fit the timeframe of the sentence.
+// The simple past tense \"walked\" is the only option that properly conveys that the action took place entirely in the past.
+
+// Assistant:`
+// Let AI also reveal the answer and explain, to let it create more correct questions. Later may can also use this answer to double check.
+
         messages: `\n\nHuman:
-### Create an English grammar tenses multiple choice fill-in-the-blank question.
+### Create an English grammar ${pointAndSpace}multiple choice fill-in-the-blank question.
 Reveal the correct answer after the question, explain why it is correct.
 Explain why other choices are wrong.
 Explain why you create this question.
-Add '\\n------\\n' before the question, and between the question and the explanations.
+Add '------' before the question.
+Add '------' after the choices of the question, before revealing the answer and explaining.
 
-###Example:
-Here is an English grammar verb tense multiple choice fill-in-the-blank question:
-------
-Yesterday I _____ to the store when it started raining.
-A) will walk
-B) walked
-C) had walked
-D) have walked
-------
-The correct answer is B) walked. This is the correct answer because the sentence is describing an action that took place in the past (yesterday) and is completed, so the simple past tense is needed.
-A) will walk is incorrect because it is the future tense, referring to an action that will take place in the future.
-C) had walked is the past perfect tense, used to describe an action that was completed before another past action. It does not fit here.
-D) have walked is the present perfect tense, used to describe an action that started in the past but continues to or impacts the present. It does not fit the timeframe of the sentence.
-The simple past tense \"walked\" is the only option that properly conveys that the action took place entirely in the past.
+Assistant:`
+// Let AI also reveal the answer and explain, to let it create more correct questions. Later may can also use this answer to double check.
+// Don't provide example to prevent AI almost always create questions start with something like "By the time ...".
 
-Assistant:` // Let AI also reveal the answer and explain, to let it create more correct questions. Later may can also use this answer to double check.
     }
+    console.log('------ _handleCreateQuestionSkill prompt before await:', context.messages)
     const model = window.models.CreateModel('english_practice:check_answer')
     window.models.ApplyContextObject(model, context);
     const response = await window.models.CallModel(model);
-    console.log('------ _handleCreateQuestionSkill(event) response:', response)
+    console.log('------ _handleCreateQuestionSkill prompt:', context.messages)
+    console.log('------ _handleCreateQuestionSkill response:', response)
     // console.log(response.completion.trim())
     // debugger
     // return;
@@ -62,13 +89,16 @@ Assistant:` // Let AI also reveal the answer and explain, to let it create more 
     const lines = question.split('\n');
     lines.forEach(line => {
         if (line.trim() === '') return;
+        console.log('--- line:', line, event)
         window.hooks.emit('moemate_core:handle_skill_text', {name: event.name, value: line});
+        // window.skills_llm.CallSkill(model, 'TEXT', {name: event.name, value: line});
     })
 
     // setTimeout(() => {
     //     window.hooks.emit("hack_delay", `You created this question {${question}}, don't answer the question, just write the question (keep the blank!) and choices as is, don't change anything, don't speak anything else!`);
     // }, 100);
 
+    window.models.DestroyModel(model);
     const isStop = true;
     return isStop;
 }
@@ -122,6 +152,7 @@ Assistant:`,
     const model = window.models.CreateModel('english_practice:check_answer')
     window.models.ApplyContextObject(model, context);
     const response = await window.models.CallModel(model);
+    console.log('------ _handleCheckAnswerSkill prompt:', context.messages)
     console.log('------  _handleCheckAnswerSkill response:', response.completion)
     // window.companion.SendMessage({ type: "CHECK_ANSWER", user: event.name, value: response.completion.trim(), timestamp: Date.now(), alt: 'alt'});
 
@@ -131,6 +162,7 @@ Assistant:`,
         window.hooks.emit('moemate_core:handle_skill_text', {name: event.name, value: line});
     })
 
+    window.models.DestroyModel(model);
     const isStop = true;
     return isStop;
 }
@@ -144,5 +176,5 @@ export function init() {
     window.hooks.on('english_practice:handle_create_question_skill', _handleCreateQuestionSkill)
     window.hooks.on('english_practice:handle_check_answer_skill', _handleCheckAnswerSkill)
     // window.hooks.on("set_prompts", ({model, type}) => _handleSetPrompts(model, type));
-    // window.components.AddComponentToScreen('chat-input', 'PromptSelector');
+    window.components.AddComponentToScreen('chat-input', 'PromptSelector');
 }
